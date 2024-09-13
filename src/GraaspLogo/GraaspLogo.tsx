03ac7cdeccdf4b00e324ccb6c04a7b1e7e81ca73
LAPTOP-YANNICK\ymijs <y.mijsters@gmail.com>
2024-09-13 21:40:49 +0200

    feat: add epfl logo

--- src/GraaspLogo/GraaspLogo.tsx
+++ src/GraaspLogo/GraaspLogo.tsx
@@ -4,7 +4,10 @@ type Props = {
 
 const GraaspLogo = ({height}: Props): JSX.Element => {
   return (
-    <img alt="LNCO Logo" height={height} src="/lnco-logo.png" />
+    <>
+      <img alt="LNCO Logo" height={height} src="/epfl-logo.svg" />
+      <img alt="LNCO Logo" height={height} src="/lnco-logo.png" />
+    </>
   );
 };
 
